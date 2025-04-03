6d89ed1efa17c739d93e590e9e731b4d34ed0564
4512yasir <yasiryaska1@gmail.com>
2025-04-03 11:50:54 +0300

    selection of elements

---
+++ index.js
@@ -0,0 +1,9 @@
+// Select necessary elements
+const transactionForm = document.querySelector('.transaction-form form');
+const transactionList = document.querySelector('.transaction-list ul');
+const incomeSummary = document.querySelector('.income-summary p');
+const expenseSummary = document.querySelector('.expense-summary p');
+const netBalance = document.querySelector('.net-balance p');
+const financialAdvice = document.querySelector('.financial-advice');
+const incomeGoalInput = document.getElementById('income-goal');
+const expenseGoalInput = document.getElementById('expense-goal');
